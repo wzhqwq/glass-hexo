3d356518f65d13b1111d34323ce56c064d2c802f
wzhqwq <wzhii345@outlook.com>
2020-12-03 23:21:15 +0800

    实现了点击随机动作，添加了加载提示

--- source/js/widgets.js
+++ source/js/widgets.js
@@ -3,6 +3,8 @@ window.addEventListener('load', () => {
     e.stopPropagation();
     $('#load-live2d-btn').blur();
     $('#live2d-loading')[0].className = 'show';
+    $('#live2d-msg1').text('正在加载Live2D');
+    $('#live2d-msg2').text('下载支持程序(268KB)');
     // await $.getScript('/js/live2d.js').promise;
     var scriptLoader = document.createElement('script');
     scriptLoader.src = '/js/live2d.js';
@@ -11,11 +13,15 @@ window.addEventListener('load', () => {
     coreLoader.src = '/js/live2d.core.js';
     document.head.appendChild(coreLoader);
     const next = () => {
-      if (typeof loadLive2d == 'undefined' || typeof Live2DCubismCore == 'undefined') {
+      if (typeof loadLive2d == 'undefined') {
         setTimeout(next, 500);
         return;
       }
-      loadLive2d($('#live2d').css('display', 'block')[0], live2d_path).then(() => {
+      loadLive2d(
+        $('#live2d').css('display', 'block')[0],
+        live2d_path,
+        msg => $('#live2d-msg2').text(msg)
+      ).then(() => {
         $('#live2d-not-loaded').css('display', 'none');
         $('#live2d-loading')[0].className = '';
         init();
